dcf56642c1622461d5837589c320010de1cfe5ee
Christopher Mejia <christophermejia111@gmail.com>
2024-04-22 16:07:23 -0400

    fixed routing issues and unused imports

--- src/pages/generateQuestionsPage.tsx
+++ src/pages/generateQuestionsPage.tsx
@@ -1,8 +1,6 @@
-import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import Navbar from '../components/navbar';
 import openai from '../lib/openaiAPI';
-import { supabase } from '@supabase/auth-ui-shared';
 
 function GenerateQuestions() {
     const [input, setInput] = useState("");
@@ -33,7 +31,7 @@ function GenerateQuestions() {
             <div className="flex flex-col justify-center items-center space-y-12">
                 <h1 className="mb-4 text-4xl font-mono leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Enter a job description</h1>
                 <textarea value={input} onChange={handleInputChange} className="resize-none py-3 px-4 block w-96 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none outline outline-1"></textarea>
-                <button onClick={getQuestions}>Generate Questions</button>
+                <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={getQuestions}>Generate Questions</button>
             </div>
         </div>
     )
